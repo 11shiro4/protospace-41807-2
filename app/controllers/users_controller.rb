986ee63e64592383b6f03f44f20382c5fd46daac
11shiro4 <yumako345@gmail.com>
2024-12-11 09:46:10 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,5 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :update, :destroy]
-  #before_action :set_user, only: :show
-
-  def edit
-  end
-
-  def new
-    @user = User.new
-  end
 
   def show
     @user = User.find(params[:id])
@@ -16,28 +8,7 @@ class UsersController < ApplicationController
     @user_profile = @user.profile
     @user_occupation = @user.occupation
     @user_position = @user.position
-  end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to @user
-    else
-      @user.image.purge if @user.image.attached?
-      render "new"
-    end
-  end#create全て追記
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(post_params)
-      redirect_to @user
-    else
-      # 保持したいデータはそのまま、画像は再入力を促す
-      @user.image.purge if @user.image.attached?
-      render :edit
-    end
-  end
 
   private
 
